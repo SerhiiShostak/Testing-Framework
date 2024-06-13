d461da43f9d7aa3013c0d5a1a35cf61f38f31049
SadKoldun <serezhashostak9@gmail.com>
2024-06-13 17:24:09 +0300

    Added headless mode for Chromedriver

--- modules/ui/page_object/base_page.py
+++ modules/ui/page_object/base_page.py
@@ -6,9 +6,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 class BasePage:
 
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+
     def __init__(self):
         self.driver = webdriver.Chrome(
-            service=Service(ChromeDriverManager().install())
+            service=Service(ChromeDriverManager().install()), options=BasePage.options
         )
 
     def close(self):
